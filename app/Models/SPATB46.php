ab948707e7694a6a6b944bca4ef65b0806f7de98
AbigaelleT <102353251+AbigaelleT@users.noreply.github.com>
2023-05-11 21:59:20 +0000

    Add files via upload

---
+++ app/Models/SPATB46.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+//table Action
+class SPATB46 extends Model
+{
+    use HasFactory;
+}
