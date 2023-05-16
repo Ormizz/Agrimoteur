170deb298296f0027119e6913fe8d44202734122
AbigaelleT <Tmaissanah@gmail.com>
2023-05-16 22:44:58 +0000

    hey

---
+++ app/Http/Livewire/SPAIN02/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\SPAIN02;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.s-p-a-i-n02.edit');
+    }
+}
